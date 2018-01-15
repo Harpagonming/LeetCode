b3b71270c53c6a42816d8f432e53dfd4cfc2fce9
Harpagon <zhenming1989@sina.com>
2018-01-15 16:28:55 +0800

    增加测试用main

--- src/algorithms/Main.java
+++ src/algorithms/Main.java
@@ -1,4 +1,12 @@
 package algorithms;
 
+import algorithms.easy.FindAnagramMappings.Solution;
+
 public class Main {
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    int[] A = {12, 28, 46, 32, 50};
+    int[] B = {50, 12, 32, 46, 28};
+    System.out.println(solution.anagramMappings(A, B));
+  }
 }
