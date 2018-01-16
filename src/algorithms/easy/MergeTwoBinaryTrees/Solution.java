d04bd28f0f87e2103eae5bd09427b85a21a3d33e
Harpagon <zhenming1989@sina.com>
2018-01-16 14:46:39 +0800

    MergeTwoBinaryTrees修改BUG

--- src/algorithms/easy/MergeTwoBinaryTrees/Solution.java
+++ src/algorithms/easy/MergeTwoBinaryTrees/Solution.java
@@ -12,26 +12,16 @@ public class Solution {
   }
 
   public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-    TreeNode t = new TreeNode(t1.val + t2.val);
-    if (t1.left == null && t2.left == null) {
-      t.left = null;
-    } else if (t1.left == null) {
-      t.left = t2.left;
-    } else if (t2.left == null) {
-      t.left = t1.left;
-    } else {
-      t.left = mergeTrees(t1.left, t2.left);
+    if (t1 == null && t2 == null) {
+      return null;
     }
+    int val = (t1 == null ? 0 : t1.val) + (t2 == null ? 0 : t2.val);
 
-    if (t1.right == null && t2.right == null) {
-      t.right = null;
-    } else if (t1.right == null) {
-      t.right = t2.right;
-    } else if (t2.right == null) {
-      t.right = t1.right;
-    } else {
-      t.right = mergeTrees(t1.right, t2.right);
-    }
+    TreeNode t = new TreeNode(val);
+
+    t.left = mergeTrees(t1 == null ? null : t1.left, t2 == null ? null : t2.left);
+    t.right = mergeTrees(t1 == null ? null : t1.right, t2 == null ? null : t2.right);
+
     return t;
   }
 
