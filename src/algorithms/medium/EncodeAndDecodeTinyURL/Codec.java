bbd5c8b505c049e23b19a2cdfbd1b0d00f6d0eeb
Harpagon <zhenming1989@sina.com>
2018-01-16 17:28:12 +0800

    EncodeAndDecodeTinyURL解决

--- src/algorithms/medium/EncodeAndDecodeTinyURL/Codec.java
+++ src/algorithms/medium/EncodeAndDecodeTinyURL/Codec.java
@@ -1,4 +1,37 @@
 package algorithms.medium.EncodeAndDecodeTinyURL;
 
-public class CodeC {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Codec {
+  private static Map<String, String> longUrlIndex = new HashMap<>();
+  private static Map<String, String> shortUrlIndex = new HashMap<>();
+
+  private static String BASE = "http://tinyurl.com/";
+
+  // Encodes a URL to a shortened URL.
+  public String encode(String longUrl) {
+    if (longUrlIndex.containsKey(longUrl)) {
+      return longUrlIndex.get(longUrl);
+    }
+    String charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    StringBuilder sb;
+    do {
+      sb = new StringBuilder("");
+      for (int i = 0; i < 6; i++) {
+        sb.append(charSet.charAt((int) (Math.random() * charSet.length())));
+      }
+    } while (shortUrlIndex.containsKey(sb.toString()));
+    shortUrlIndex.put(sb.toString(), longUrl);
+    return BASE + sb.toString();
+  }
+
+  // Decodes a shortened URL to its original URL.
+  public String decode(String shortUrl) {
+    return shortUrlIndex.get(shortUrl.replace(BASE, ""));
+  }
 }
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
