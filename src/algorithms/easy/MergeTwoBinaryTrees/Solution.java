2a3091f5407539fd52d4cf934befd83a4232b570
Harpagon <zhenming1989@sina.com>
2018-01-16 16:47:54 +0800

    MergeTwoBinaryTrees修改

--- src/algorithms/easy/MergeTwoBinaryTrees/Solution.java
+++ src/algorithms/easy/MergeTwoBinaryTrees/Solution.java
@@ -12,17 +12,16 @@ public class Solution {
   }
 
   public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-    if (t1 == null && t2 == null) {
-      return null;
+    if (t1 == null) {
+      return t2;
+    } else if (t2 == null) {
+      return t1;
     }
-    int val = (t1 == null ? 0 : t1.val) + (t2 == null ? 0 : t2.val);
 
-    TreeNode t = new TreeNode(val);
-
-    t.left = mergeTrees(t1 == null ? null : t1.left, t2 == null ? null : t2.left);
-    t.right = mergeTrees(t1 == null ? null : t1.right, t2 == null ? null : t2.right);
+    TreeNode t = new TreeNode(t1.val + t2.val);
+    t.left = mergeTrees(t1.left, t2.left);
+    t.right = mergeTrees(t1.right, t2.right);
 
     return t;
   }
-
 }
---
+++ src/algorithms/medium/EncodeAndDecodeTinyURL/Codec.java
@@ -0,0 +1,4 @@
+package algorithms.medium.EncodeAndDecodeTinyURL;
+
+public class CodeC {
+}
