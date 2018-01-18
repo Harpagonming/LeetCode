d661fee83bcf9ec2520f4546594699b94161ed2a
Harpagon <zhenming1989@sina.com>
2018-01-18 17:40:11 +0800

    MaximumBinaryTree解决

--- src/algorithms/medium/MaximumBinaryTree/Solution.java
+++ src/algorithms/medium/MaximumBinaryTree/Solution.java
@@ -1,4 +1,17 @@
 package algorithms.medium.MaximumBinaryTree;
 
 public class Solution {
+  class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+
+  public TreeNode constructMaximumBinaryTree(int[] nums) {
+    return null;
+  }
 }
