816135a0886013cbf040ce608b99342d8d15561b
Harpagon <zhenming1989@sina.com>
2018-07-09 13:41:03 +0800

    .gitignore文件修改

--- src/algorithms/Main.java
+++ src/algorithms/Main.java
@@ -7,6 +7,6 @@ public class Main {
     Solution solution = new Solution();
     int[] A = {12, 28, 46, 32, 50};
     int[] B = {50, 12, 32, 46, 28};
-    System.out.println(solution.anagramMappings(A, B));
+    System.out.println(solution.anagramMappings(A, B).toString());
   }
 }
